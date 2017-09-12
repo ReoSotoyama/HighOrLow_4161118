63a6a42926544854f76e1925f38a1d83ee374a74
Reo Sotoyama <reo.sotoyama.sys@gmail.com>
2017-09-12 11:33:23 +0900

    メインの処理を追加 #3

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
 
@@ -5,6 +8,20 @@ public class Main {
 		int answer=(int)(Math.random()*100)+1;  // 正解を作る
 	    int number=0;                           // 入力数
 	    int count;
+
+	    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("1から100までの間だよ。");
+
+        for(count=0;number!=answer;count++){
+          System.out.print("数字を入れてね : ");
+          Scanner s = new Scanner(System.in);
+          number=Integer.parseInt(s.next());
+          if(number<answer)
+            System.out.println("もっと上だよ。");
+          else if(number>answer)
+            System.out.println("もっと下だよ。");
+        }
+        System.out.println("当たり！！  "+count+"回でした。");
 	}
 
 }
