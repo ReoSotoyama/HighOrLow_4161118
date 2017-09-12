6693636be094991f759074ee6b8d61a4473481a8
Reo Sotoyama <reo.sotoyama.sys@gmail.com>
2017-09-12 11:25:46 +0900

    使用する変数の設定 #1

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int answer=(int)(Math.random()*100)+1;  // 正解を作る
+	    int number=0;                           // 入力数
+	    int count;
+	}
+
+}
